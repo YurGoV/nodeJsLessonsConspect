c9f1d0f89770ae087030cc09b59e0cca406de575
YurGoV <yurgov@gmail.com>
2023-03-12 18:33:44 +0200

    end of lesson 1 (part two)

--- 02/index.js
+++ 02/index.js
@@ -9,14 +9,18 @@
 
 //todo to bookmark: request to entering text
 const readline = require("readline");
-const fs = require("fs").promices;
-const commander = require("commander");
+const fs = require("fs").promises;
+const { program } = require("commander");
 require("colors");
+
+program.option('-f, --file [type]', 'file for saving results', 'game_results.txt');
+program.parse(process.argv);
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
+
 
 /* rl.on('line', (txt) => {
     console.log(`You entered this text: ${txt}`);
@@ -25,21 +29,69 @@ const rl = readline.createInterface({
 
 let counter = 0;
 const mind = Math.ceil(Math.random() * 10);
+const logFile = program.opts().file;
 
-/* Gussied number */
+/**
+ * Simple input data validator
+ * @param {number} val
+ * @returns {boolean}
+ */
+const isValid = (val) => {
+  if (!isNaN(val) && val >= 1 && val <= 10) return true;
+
+  if (isNaN(val)) console.log("Please, enter a number".red);
+  if (val < 1 || val > 10) console.log("nimber must be between 1 and 10".red);
+
+  return false;
+};
+
+/**
+ * Log game results to the txt file
+ * @param {string} msg - message to log
+ * @returns {Promise<void>}
+ */
+const logger = async (msg) => {
+try {
+
+  await fs.appendFile(logFile, `${msg}\n`)
+
+  console.log(`game results successfully saved to ${logFile}`.yellow);
+} catch (err) {
+  console.log(err.message);
+}
+}
+
+/**
+ * Main game process
+ */
 const game = () => {
   rl.question(
     "Please enter any whole number between 1 and 10!\n".blue,
     (val) => {
       const number = +val;
 
+      if (!isValid(number)) return game();
+
       counter += 1;
 
-      if()
+      if (number !== mind) {
+        console.log(" oh no, try again".red);
+
+        return game();
+        // OR:
+        // if(number !== mind) return game();
+      }
+
+      console.log(
+        `Congrats! you guessed the number in ${counter} step(s) =^^=`.magenta
+      );
+      logger(`${new Date().toLocaleString('uk-UA')} Congrats! you guessed the number in ${counter} step(s) =^^=`)
+
+      rl.close();
     }
   );
 };
-
+6;
 game();
 
-console.log(":C:L:G: ~ file: index.js:27 ~ mind:", mind);
+// console.log(":CL: ~ file: index.js:27 ~ mind:", mind);
