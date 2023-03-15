b29b97670c6194def8dc8a30aa219aa622d16791
Yuriy <yurgov@gmail.com>
2023-03-15 16:40:52 +0200

    less1

--- 03/server.js
+++ 03/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const fs = require('fs').promises;
-const { v4: uuidv4 } = require('uuid');
+const { v4: uuid } = require('uuid');
 
 const app = express();
 
@@ -19,22 +19,26 @@ app.use(express.json());
  */
 
 /**
- * create user
+ * * create user
  */
 app.post('/api/v1/user', async (req, res) => {
   try {
-    console.log(req.body);
+    // console.log(req.body);
+    const { name, year } = req.body;
 
     const dataFromDb = await fs.readFile('./03/models.json');
     const users = JSON.parse(dataFromDb);
-    // users.push({
-    //   id
-    // })
+    const newUser = {
+      id: uuid(),
+      name,
+      year,
+    };
+    users.push(newUser);
 
     await fs.writeFile('./03/models.json', JSON.stringify(users));
 
     res.status(200).json({
-      'req.body is:': req.body,
+      user: newUser,
     });
   } catch (err) {
     res.status(500).json({
@@ -43,10 +47,47 @@ app.post('/api/v1/user', async (req, res) => {
   }
 });
 
-app.get('/api/v1/user', (req, res) => {
-  res.status(200).json({
-    'req.time is:': req.time,
-  });
+/**
+ * * GET all users
+ */
+app.get('/api/v1/user', async (req, res) => {
+  try {
+    const usersString = await fs.readFile('./03/models.json');
+    const users = JSON.parse(usersString);
+    res.status(200).json({
+      users,
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: err.message,
+    });
+  }
+});
+
+/**
+ * * GET user by id
+ */
+app.get('/api/v1/user/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const usersString = await fs.readFile('./03/models.json');
+    const users = await JSON.parse(usersString);
+    // const users = JSON.parse(await fs.readFile('./03/models.json'));
+    console.log(':CL: -----------------------------------------:CL:');
+    console.log(':CL: ~ file: server.js:75 ~ users:', users);
+    console.log(':CL: -----------------------------------------:CL:');
+
+    const foundedUser = await users.find((item) => item.id === id);
+    console.log(':CL: ~ file: server.js:76 ~ foundedUser:', foundedUser);
+
+    res.status(200).json({
+      users,
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: err.message,
+    });
+  }
 });
 
 app.get('*', (req, res) => {
